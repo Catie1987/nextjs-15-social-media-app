eb2f2c4e95a39bbc6ed5661e0a575f84675922f7
Catie1987 <hieu.nguyen912@gmail.com>
2026-05-24 10:56:09 +0700

    Update PostEditor.tsx

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -11,7 +11,7 @@ import StarterKit from "@tiptap/starter-kit";
 import { useDropzone } from "@uploadthing/react";
 import { ImageIcon, Loader2, X, SmilePlus } from "lucide-react";
 import Image from "next/image";
-import { ClipboardEvent, useEffect, useMemo, useState } from "react";
+import { ClipboardEvent, useEffect, useMemo, useState, useRef } from "react";
 import { useSubmitPostMutation } from "./mutations";
 import "./styles.css";
 import useMediaUpload, { Attachment } from "./useMediaUpload";
